d308b7fb15701a994dc8c41b454e6fde61afab20
S M Khurram Khan <smkhurramkhan@outlook.com>
2022-04-23 02:53:48 +0500

    kotlin conversion

--- app/src/main/java/com/example/customnotifications/AlarmReceiver.kt
+++ app/src/main/java/com/example/customnotifications/AlarmReceiver.kt
@@ -1,77 +1,68 @@
-package com.example.customnotifications;
+package com.example.customnotifications
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 
-import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationCompat;
-
-public class AlarmReceiver extends BroadcastReceiver {
-    public String Channelid = "myChannel";
-    public NotificationManager nm;
-    public NotificationChannel mChannel;
-    PendingIntent pendingIntenthoro;
-    SharedPrefs sharedPrefs;
+class AlarmReceiver : BroadcastReceiver() {
+    var Channelid = "myChannel"
+    var nm: NotificationManager? = null
+    var mChannel: NotificationChannel? = null
+    var pendingIntenthoro: PendingIntent? = null
+    var sharedPrefs: SharedPrefs? = null
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        sharedPrefs = new SharedPrefs(context);
-        nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        Intent mynew = new Intent(context, MainActivity.class);
-
-        pendingIntenthoro = PendingIntent.getActivity(context, 0, mynew,
-                PendingIntent.FLAG_IMMUTABLE);
-
-
+    override fun onReceive(context: Context, intent: Intent) {
+        sharedPrefs = SharedPrefs(context)
+        nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val mynew = Intent(context, MainActivity::class.java)
+        pendingIntenthoro = PendingIntent.getActivity(
+            context, 0, mynew,
+            PendingIntent.FLAG_IMMUTABLE
+        )
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (!sharedPrefs.getNotification()) {
-
-                CharSequence name = "myChannel1";
-                int importance = NotificationManager.IMPORTANCE_HIGH;
-                mChannel = new NotificationChannel(Channelid, name, importance);
-                nm.createNotificationChannel(mChannel);
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, Channelid);
-                builder.setAutoCancel(true);
-                builder.setSmallIcon(R.mipmap.ic_launcher);
-                builder.setContentTitle("Horoscope Updates");
-                builder.setContentText("Tap to see daily horoscope.");
-                builder.setContentIntent(pendingIntenthoro);
-                builder.setOngoing(false);
-                nm.notify(1001, builder.build());
-
-
+            if (!sharedPrefs!!.getNotification()) {
+                val name: CharSequence = "myChannel1"
+                val importance = NotificationManager.IMPORTANCE_HIGH
+                mChannel = NotificationChannel(Channelid, name, importance)
+                nm!!.createNotificationChannel(mChannel!!)
+                val builder = NotificationCompat.Builder(context, Channelid)
+                builder.setAutoCancel(true)
+                builder.setSmallIcon(R.mipmap.ic_launcher)
+                builder.setContentTitle("Horoscope Updates")
+                builder.setContentText("Tap to see daily horoscope.")
+                builder.setContentIntent(pendingIntenthoro)
+                builder.setOngoing(false)
+                nm!!.notify(1001, builder.build())
             } else {
-                CharSequence name = "myChannel1";
-                int importance = NotificationManager.IMPORTANCE_HIGH;
-                mChannel = new NotificationChannel(Channelid, name, importance);
-                nm.createNotificationChannel(mChannel);
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, Channelid);
-                builder.setAutoCancel(true);
-                builder.setSmallIcon(R.mipmap.ic_launcher);
-                builder.setContentTitle(context.getString(R.string.app_name));
-                builder.setContentText("This is your custom notification.");
-                builder.setContentIntent(pendingIntenthoro);
-                builder.setOngoing(false);
-                nm.notify(1001, builder.build());
+                val name: CharSequence = "myChannel1"
+                val importance = NotificationManager.IMPORTANCE_HIGH
+                mChannel = NotificationChannel(Channelid, name, importance)
+                nm!!.createNotificationChannel(mChannel!!)
+                val builder = NotificationCompat.Builder(context, Channelid)
+                builder.setAutoCancel(true)
+                builder.setSmallIcon(R.mipmap.ic_launcher)
+                builder.setContentTitle(context.getString(R.string.app_name))
+                builder.setContentText("This is your custom notification.")
+                builder.setContentIntent(pendingIntenthoro)
+                builder.setOngoing(false)
+                nm!!.notify(1001, builder.build())
             }
         } else {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, Channelid);
-            builder.setAutoCancel(true);
-            builder.setSmallIcon(R.mipmap.ic_launcher);
-            builder.setContentTitle(context.getString(R.string.app_name));
-            builder.setContentText("This is your custom notification.");
-            builder.setContentIntent(pendingIntenthoro);
-            builder.setOngoing(false);
-            nm.notify(1001, builder.build());
-
+            val builder = NotificationCompat.Builder(context, Channelid)
+            builder.setAutoCancel(true)
+            builder.setSmallIcon(R.mipmap.ic_launcher)
+            builder.setContentTitle(context.getString(R.string.app_name))
+            builder.setContentText("This is your custom notification.")
+            builder.setContentIntent(pendingIntenthoro)
+            builder.setOngoing(false)
+            nm!!.notify(1001, builder.build())
         }
     }
-
 }
--- app/src/main/java/com/example/customnotifications/BootCompleteNotificationPublisher.kt
+++ app/src/main/java/com/example/customnotifications/BootCompleteNotificationPublisher.kt
@@ -1,78 +1,64 @@
-package com.example.customnotifications;
+package com.example.customnotifications
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import java.util.*
 
-import java.util.Calendar;
-import java.util.Date;
-
-public class BootCompleteNotificationPublisher extends BroadcastReceiver {
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        SharedPrefs sharedPrefs  =new SharedPrefs(context);
-        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+class BootCompleteNotificationPublisher : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val sharedPrefs = SharedPrefs(context)
+        if (intent.action == "android.intent.action.BOOT_COMPLETED") {
             if (!sharedPrefs.getNotification()) {
-                    Intent alarmIntent = new Intent(context, AlarmReceiver.class);
-                    alarmIntent.putExtra("sender2", "horoscope");
-                    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent,
-                            PendingIntent.FLAG_IMMUTABLE);
+                val alarmIntent = Intent(context, AlarmReceiver::class.java)
+                alarmIntent.putExtra("sender2", "horoscope")
+                val pendingIntent = PendingIntent.getBroadcast(
+                    context, 0, alarmIntent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+                val manager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-                    AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-
-     /*               Date date = new Date();
+                /*               Date date = new Date();
                     //Calendar cal_now = Calendar.getInstance();
                     //cal_now.setTime(date);*/
-
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.setTimeInMillis(System.currentTimeMillis());
-                    calendar.set(Calendar.HOUR_OF_DAY, 10);
-                    calendar.set(Calendar.MINUTE, 0);
-                    calendar.set(Calendar.SECOND, 1);
-
-
-                    if (calendar.before(Calendar.getInstance())) {
-                        calendar.add(Calendar.DATE, 1);
-                    }
-
-                    if (manager != null) {
-                        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                                AlarmManager.INTERVAL_DAY, pendingIntent);
-                    }
-
+                val calendar = Calendar.getInstance()
+                calendar.timeInMillis = System.currentTimeMillis()
+                calendar[Calendar.HOUR_OF_DAY] = 10
+                calendar[Calendar.MINUTE] = 0
+                calendar[Calendar.SECOND] = 1
+                if (calendar.before(Calendar.getInstance())) {
+                    calendar.add(Calendar.DATE, 1)
+                }
+                manager?.setInexactRepeating(
+                    AlarmManager.RTC_WAKEUP, calendar.timeInMillis,
+                    AlarmManager.INTERVAL_DAY, pendingIntent
+                )
             } else {
 
                 // on device boot complete, reset the alarm
-
-                Intent alarmIntent = new Intent(context, AlarmReceiver.class);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent,
-                        PendingIntent.FLAG_IMMUTABLE);
-
-                AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-
-                Date date = new Date();
+                val alarmIntent = Intent(context, AlarmReceiver::class.java)
+                val pendingIntent = PendingIntent.getBroadcast(
+                    context, 0, alarmIntent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+                val manager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                val date = Date()
                 //Calendar cal_now = Calendar.getInstance();
                 //cal_now.setTime(date);
-
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-                calendar.set(Calendar.HOUR_OF_DAY, 10);
-                calendar.set(Calendar.MINUTE, 0);
-                calendar.set(Calendar.SECOND, 1);
-
-
+                val calendar = Calendar.getInstance()
+                calendar.timeInMillis = System.currentTimeMillis()
+                calendar[Calendar.HOUR_OF_DAY] = 10
+                calendar[Calendar.MINUTE] = 0
+                calendar[Calendar.SECOND] = 1
                 if (calendar.before(Calendar.getInstance())) {
-                    calendar.add(Calendar.DATE, 1);
+                    calendar.add(Calendar.DATE, 1)
                 }
-
-                if (manager != null) {
-                    manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                            AlarmManager.INTERVAL_DAY, pendingIntent);
-                }
+                manager?.setInexactRepeating(
+                    AlarmManager.RTC_WAKEUP, calendar.timeInMillis,
+                    AlarmManager.INTERVAL_DAY, pendingIntent
+                )
             }
         }
     }
